8d9b95e18dd5d5d598011bdb84a162aa47db9ecd
greybeard <greybeard@gmail.com>
2024-06-12 13:20:07 +0300

    AirBnB clone2

--- models/user.py
+++ models/user.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
-from models import storage_t
-from sqlalchemy import Column, String, DateTime
-from models.base_model import Base
+""" holds class User"""
 
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
+#
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    if storage_t == "db":
-        __tablename__ = "users"
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -23,5 +27,11 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes User"""
+        """initializes user"""
         super().__init__(*args, **kwargs)
+
+    #def __setattr__(self, name, value):
+    #    """sets a password with md5 encryption"""
+    #    if name == "password":
+    #        value = md5(value.encode()).hexdigest()
+    #    super().__setattr__(name, value)
