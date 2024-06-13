ca77bba92fcdcc6afbe1d0c8f0aedc8bfe9518a5
greybeard <greybeard@gmail.com>
2024-06-13 07:26:51 +0300

    states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -22,12 +22,17 @@ def cities_by_states():
     """display a HTML page: (inside the tag BODY"""
     states = storage.all(State).values()
     cities = storage.all(City).values()
-    return render_template('8-cities_by_states.html', states=states, cities=cities)
+    return render_template(
+            '8-cities_by_states.html',
+            states=states,
+            cities=cities)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
